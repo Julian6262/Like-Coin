eca382988e7501c7d870cdce15052f7b500eb9bf
Julian6262 <d.baburin62@gmail.com>
2023-08-19 01:27:11 +0300

    footer + header + top

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,15 @@
-$(function () {
+// MODAL////////////////////////////////////////////////
+const modal = document.querySelector('.modal');
 
-})
+document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("footer__rules")) {
+        modal.classList.add('modal--active');
+    }
+
+    if (e.target.classList.contains("modal") || e.target.classList.contains("modal__svg")) {
+        modal.classList.remove('modal--active');
+    }
+});
+
+
+
