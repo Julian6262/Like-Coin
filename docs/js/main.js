84d38a26e44297ecfee24a4fab1d5d9d82a68f12
Julian6262 <d.baburin62@gmail.com>
2023-08-25 08:57:09 +0300

    calculator js

--- docs/js/main.js
+++ docs/js/main.js
@@ -5,7 +5,7 @@ document.addEventListener("click", function (e) {
     if (e.target.classList.contains("footer__rules")) {
         modal.classList.add('modal--active');
     }
-
+    
     if (e.target.classList.contains("modal") || e.target.classList.contains("modal__svg")) {
         modal.classList.remove('modal--active');
     }
@@ -33,30 +33,41 @@ menu.forEach((item) => {
 
 
 // CALCULATOR
+const multiplier = 0.0000121832359;
+const multiplierDay = 86400;
+
 const value = document.querySelector("#calc-value");
 const input = document.querySelector("#calc-input");
+const speedUp = document.querySelector("#sp-up");
+const day = document.querySelector("#day");
+const month = document.querySelector("#month");
+const threeMonth = document.querySelector("#threeMonth");
+const sixMonth = document.querySelector("#sixMonth");
+
+function calculate() {
+    speedUp.value = (Math.round((input.value * multiplier) * 10000000) / 10000000).toFixed(7);
+    day.value = (speedUp.value * multiplierDay).toFixed(2);
+    month.value = (speedUp.value * multiplierDay * 30).toFixed(2);
+    threeMonth.value = (speedUp.value * multiplierDay * 30 * 3).toFixed(2);
+    sixMonth.value = (speedUp.value * multiplierDay * 30 * 6).toFixed(2);
+}
 
 value.value = input.value;
 
 input.addEventListener("input", (event) => {
     value.value = event.target.value;
+    calculate();
 });
+
 value.addEventListener("input", (event) => {
 
-    event11 = event.target.value;
-    console.log(event11);
-    console.log(typeof event11);
-
-    if (typeof event11) {
-        if (event.target.value > 50000) {
-            event.target.value = 50000;
-        }
-        if (event.target.value < 100) {
-            event.target.value = 100;
-        }
+    if (event.target.value > 50000) {
+        event.target.value = 50000;
     }
 
-
     input.value = event.target.value;
-
+    calculate();
 });
+
+
+
